eb5a9b8a5b901ddbab066e2d9255c0e33f2ac7d3
Alex Gonzalez <alex.gonzalez@campspot.com>
2021-07-20 15:57:15 -0600

    enabled fargate

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "create_ecs" {
   description = "Controls if ECS should be created"
   type        = bool
-  default     = true
+  default     = false
 }
 variable "name" {
   description = "Name to be used on all the resources as identifier, also the name of the ECS cluster"
@@ -16,18 +16,17 @@ variable "tags" {
     customer = "",
     environment = "",
     costcenter = "",
-    application = ""
   }
 }
 variable "capacity_providers" {
   description = "List of short names of one or more capacity providers to associate with the cluster. Valid values also include FARGATE and FARGATE_SPOT."
   type        = list(string)
-  default     = []
+  default     = ["FARGATE_SPOT", "FARGATE"]
 }
 variable "default_capacity_provider_strategy" {
   description = "The capacity provider strategy to use by default for the cluster. Can be one or more."
   type        = list(map(any))
-  default     = []
+  default     = ["FARGATE_SPOT"]
 }
 variable "container_insights" {
   description = "Controls if ECS Cluster has container insights enabled"
